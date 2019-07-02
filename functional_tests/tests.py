49af51e6c1b88743fd5e0922855dbde35bf7ac5e
Shabie Iqbal <shabie.iqbal@daimler.com>
2019-07-02 08:13:50 +0200

    Add an explicit incremental wait in FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,14 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 chromeOptions = webdriver.ChromeOptions()
 chromeOptions.add_experimental_option('useAutomationExtension', False)
 chromeOptions.add_argument('--disable-gpu')
 chromeOptions.add_argument('--headless')
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):  # eliminates the need for starting the test server also
 
@@ -15,6 +17,20 @@ class NewVisitorTest(LiveServerTestCase):  # eliminates the need for starting th
 
     def tearDown(self) -> None:
         self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                # self.assertIn(row_text, [row.text for row in rows])
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def check_for_rowin_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -40,9 +56,7 @@ class NewVisitorTest(LiveServerTestCase):  # eliminates the need for starting th
         # When she hits Enter, th epage updates, and now the page lists
         # '1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_rowin_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She enteres "Use peacock feathery to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -51,10 +65,8 @@ class NewVisitorTest(LiveServerTestCase):  # eliminates the need for starting th
         time.sleep(1)
 
         # The page updates again and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_for_rowin_list_table('1: Buy peacock feathers')
-        self.check_for_rowin_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL
         # for her. There is some explanatory text to that effect.
