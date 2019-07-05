14a8d9ed543d4084ed6df1f654e2a65d21e1c2e1
Shabie Iqbal <shabie.iqbal@daimler.com>
2019-07-05 23:19:33 +0200

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -25,11 +25,18 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_templates(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
         # Setup: Create 2 items in Database
@@ -37,11 +44,11 @@ class HomePageTest(TestCase):
         Item.objects.create(text='itemey 2')
 
         # Exercise: Call the homepage up
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
         # Assert: Check if the created values are in the home page
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')  # Django specific test that knows how to find content inside
+        self.assertContains(response, 'itemey 2')  # hence eliminates the need for response.content.decode()
 
 
 class ItemModelTest(TestCase):
